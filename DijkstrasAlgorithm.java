f21124b3751bb84a5323df795598b764f3426330
mulikar26@gmail.com <mulikar26@gmail.com>
2022-11-15 12:55:24 +0530

    first commit

---
+++ DijkstrasAlgorithm.java
@@ -0,0 +1,204 @@
+
+
+// A Java program for Dijkstra's
+// single source shortest path algorithm. The program is for
+// adjacency matrix representation of the graph to find the
+// shortest path between Mumbai To Pune
+// by Amit Mulikar 0015.
+
+
+
+public class DijkstrasAlgorithm {
+
+	private static final int NO_PARENT = -1;
+
+
+
+
+	// Function that implements Dijkstra's
+	// single source shortest path
+	// algorithm for a graph represented
+	// using adjacency matrix
+	// representation
+
+	private static void dijkstra(int[][] adjacencyMatrix,
+										int startVertex)
+	{
+		int nVertices = adjacencyMatrix[0].length;
+
+		// shortestDistances[i] will hold the
+		// shortest distance from src to i
+		int[] shortestDistances = new int[nVertices];
+
+		// added[i] will true if vertex i is
+		// included / in shortest path tree
+		// or shortest distance from src to
+		// i is finalized
+		boolean[] added = new boolean[nVertices];
+
+		// Initialize all distances as
+		// INFINITE and added[] as false
+		for (int vertexIndex = 0; vertexIndex < nVertices;
+											vertexIndex++)
+		{
+			shortestDistances[vertexIndex] = Integer.MAX_VALUE;
+			added[vertexIndex] = false;
+		}
+		
+		// Distance of source vertex from
+		// itself is always 0
+		shortestDistances[startVertex] = 0;
+
+		// Parent array to store shortest
+		// path tree
+		int[] parents = new int[nVertices];
+
+		// The starting vertex does not
+		// have a parent
+		parents[startVertex] = NO_PARENT;
+
+		// Find shortest path for all
+		// vertices
+	
+
+		for (int i = 1; i < nVertices; i++)
+		{
+
+			// Pick the minimum distance vertex
+			// from the set of vertices not yet
+			// processed. nearestVertex is
+			// always equal to startNode in
+			// first iteration.
+			int nearestVertex = -1;
+			int shortestDistance = Integer.MAX_VALUE;
+			for (int vertexIndex = 0;
+					vertexIndex < nVertices;
+					vertexIndex++)
+			{
+				if (!added[vertexIndex] &&
+					shortestDistances[vertexIndex] <
+					shortestDistance)
+				{
+					nearestVertex = vertexIndex;
+					shortestDistance = shortestDistances[vertexIndex];
+				}
+			}
+
+			// Mark the picked vertex as
+			// processed
+			added[nearestVertex] = true;
+
+			// Update dist value of the
+			// adjacent vertices of the
+			// picked vertex.
+			for (int vertexIndex = 0;
+					vertexIndex < nVertices;
+					vertexIndex++)
+			{
+				int edgeDistance = adjacencyMatrix[nearestVertex][vertexIndex];
+				
+				if (edgeDistance > 0
+					&& ((shortestDistance + edgeDistance) <
+						shortestDistances[vertexIndex]))
+				{
+					parents[vertexIndex] = nearestVertex;
+					shortestDistances[vertexIndex] = shortestDistance +
+													edgeDistance;
+				}
+			}
+		}
+
+		printSolution(startVertex, shortestDistances, parents);
+	}
+
+
+
+
+
+	// A utility function to print
+	// the constructed distances
+	// array and shortest paths
+	private static void printSolution(int startVertex,
+									int[] distances,
+									int[] parents)
+	{
+		int nVertices = distances.length;
+		System.out.print("\n\n\nVertex\t\t Distance\tPath");
+		
+		for (int vertexIndex = 0;
+				vertexIndex < nVertices;
+				vertexIndex++)
+		{
+			if (vertexIndex != startVertex)
+			{
+				if (vertexIndex == 11){ // To show only Final path . Remove this if block to see path to all places.
+				System.out.print("\n" + startVertex + " -> ");
+				System.out.print(vertexIndex + "   "+"\t"+" ");
+				System.out.print(distances[vertexIndex] + "km" +"\t\t");
+				printPath(vertexIndex, parents);
+				
+				System.out.println("\n\n\n In above Solution No. representing following places \n"
+						+ "0 Mumbai\r\n"
+						+ "1 Panvel\r\n"
+						+ "2 Rasayani\r\n"
+						+ "3 Khalapur\r\n"
+						+ "4 Khopoli\r\n"
+						+ "5 Lonavala\r\n"
+						+ "6 Devghar\r\n"
+						+ "7 Kamshet\r\n"
+						+ "8 Urse\r\n"
+						+ "9 Pimpri-Chinchwad\r\n"
+						+ "10 Hingawadi\r\n"
+						+ "11 Pune\r\n"
+						+ " ");
+				
+				}
+				}
+		}
+	}
+
+
+
+
+	// Function to print shortest path
+	// from source to currentVertex
+	// using parents array
+	private static void printPath(int currentVertex,
+								int[] parents)
+	{
+		
+		// Base case : Source node has
+		// been processed
+		if (currentVertex == NO_PARENT)
+		{
+			return;
+		}
+		printPath(parents[currentVertex], parents);
+		System.out.print( currentVertex + " ");
+	}
+
+
+
+
+		// Main Code
+	public static void main(String[] args)
+	{
+		int[][] adjacencyMatrix = {
+				{0,30,0,0,0,0,0,0,0,0,0,0},
+				{30,0,12,26,0,0,0,0,0,0,0,0},
+				{0,12,0,0,0,31,0,0,0,0,0,0},
+				{0,26,0,0,8,0,0,0,0,0,0,0},
+				{0,0,0,8,0,11,0,0,0,0,0,0},
+				{0,0,31,0,11,0,10,17,0,0,0,0},
+				{0,0,0,0,0,10,0,0,21,0,0,0},
+				{0,0,0,0,0,17,0,0,0,30,0,0},
+				{0,0,0,0,0,0,21,0,0,0,16,0},
+				{0,0,0,0,0,0,0,30,0,0,0,15},
+				{0,0,0,0,0,0,0,0,16,0,0,30},
+				{0,0,0,0,0,0,0,0,0,15,30,0} 
+				};
+		dijkstra(adjacencyMatrix, 0);
+	}
+}
+
+
